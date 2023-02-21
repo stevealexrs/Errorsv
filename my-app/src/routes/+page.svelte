3a33b903cb89f5501883053f951204a2a7f7f1dd
stevealexr <116614202+stevealexr@users.noreply.github.com>
2023-02-22 00:10:22 +0800

    🎉 Initial Commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Button } from '@svelteuidev/core';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Button>
+  Settings
+</Button>
